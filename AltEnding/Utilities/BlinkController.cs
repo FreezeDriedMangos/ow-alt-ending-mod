d85e9173719f19b7ba1fb9e41a678395e30206c7
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-05-09 13:22:15 -0400

    feat: added code to modify and manually trigger the blink animation, with help from the lovely JohnCorby and Vesper

---
+++ AltEnding/Utilities/BlinkController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AltEnding.Utilities
+{
+    class BlinkController
+    {
+        private UnityEngine.Texture2D originalEyeMaskTexture;
+        private UnityEngine.Texture2D threeEyeMaskTexture;
+        private PlayerCameraEffectController PlayerCameraEffectController;
+
+        public BlinkController(PlayerCameraEffectController pcec)
+        {
+            PlayerCameraEffectController = pcec;
+            originalEyeMaskTexture = pcec._owCamera.postProcessingSettings.eyeMask.eyeMask;
+
+            // TODO: load the 3 eye texture
+        }
+
+        public void SetBlinkNumEyes(int numEyes)
+        {
+            UnityEngine.Texture2D eyeMaskTexture = null;
+        
+            switch(numEyes)
+            {
+                case 3:
+                    eyeMaskTexture = threeEyeMaskTexture;
+                    break;
+                case 4:
+                default:
+                    eyeMaskTexture = this.originalEyeMaskTexture;
+                    break;
+            }
+
+            PlayerCameraEffectController._owCamera.postProcessingSettings.eyeMask.eyeMask = eyeMaskTexture;
+        }
+
+        public void Blink()
+        {
+            PlayerCameraEffectController.OpenEyes(1, true);
+        }
+    }
+}
