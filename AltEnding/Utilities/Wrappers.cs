bc1b50fdabb36087830d7fde54c612a09fb5a51a
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-05-11 22:31:24 -0400

    feat: imported NewHorizons to use its utilities

---
+++ AltEnding/Utilities/Wrappers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using NewHorizons;
+
+namespace AltEnding.Utilities
+{
+    public static class Wrappers
+    {
+        public static GameObject SpawnGameObject(GameObject planet, Sector sector, string propToCopyPath, Vector3 position, Vector3 eulerAngles, float scale=1, bool alignWithNormal=false)
+        {
+            return AltEnding.newHorizonsAPI.SpawnObject(planet, sector, propToCopyPath, position, eulerAngles, scale, alignWithNormal);
+        }
+
+        public static Texture2D GetTexture(string filename)
+        {
+            // return NewHorizons.Utility.ImageUtilities.GetTexture(AltEnding.Instance, filename);
+            return null;
+        }
+    }
+}
