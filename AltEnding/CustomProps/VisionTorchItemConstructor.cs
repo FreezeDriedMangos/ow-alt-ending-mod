2eac9271ec0eb5f72c279f92dce78ad760c0bcda
FreezeDriedMangoes <claytonharting+github@gmail.com>
2022-05-11 20:15:19 -0400

    fix: turns out I need to enable the VistionTorchItem component too

--- AltEnding/CustomProps/VisionTorchItemConstructor.cs
+++ AltEnding/CustomProps/VisionTorchItemConstructor.cs
@@ -19,6 +19,7 @@ namespace AltEnding.CustomProps
 			// "DreamWorld_Body/Sector_DreamWorld/Sector_Underground/Sector_PrisonCell/Interactibles_PrisonCell/PrisonerSequence/VisionTorchWallSocket/Prefab_IP_VisionTorchItem"
             GameObject Prefab_IP_VisionTorchItem = nhConstructedVisionTorchItem;
             // // Prefab_IP_VisionTorchItem.GetComponent<VisionTorchItem>().mindSlideProjector.mindSlideCollection
+			Prefab_IP_VisionTorchItem.GetComponent<VisionTorchItem>().enabled = true;
 			Prefab_IP_VisionTorchItem.GetComponent<VisionTorchItem>().mindProjectorTrigger.enabled = true;
 
             // SETTING THE MUSIC
