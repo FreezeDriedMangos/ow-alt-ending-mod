ea9d3828d817363d064687b140b57652acff9d3e
Nick <nicholasconnors@hotmail.ca>
2022-06-28 18:35:31 -0400

    Move debug commands to their own file

---
+++ AltEnding/Utilities/DebugCommands.cs
@@ -0,0 +1,82 @@
+﻿using AltEnding.Utilities.Props;
+using NewHorizons.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace AltEnding.Utilities
+{
+    public static class DebugCommands
+    {
+        public static void Update()
+        {
+
+            // Have to press tilde while entering debug commands
+            if (!Keyboard.current[Key.Backquote].isPressed) return;
+
+            var spawner = GameObject.Find("Player_Body")?.GetComponent<PlayerSpawner>();
+            var inEnding = AltEnding.newHorizonsAPI.GetCurrentStarSystem() == "clay.AltEnding";
+
+            // Go to the end of the time loop
+            if (Keyboard.current[Key.Numpad0].wasReleasedThisFrame)
+            {
+                TimeLoop.SetSecondsRemaining(30);
+            }
+
+            // Teleport to QM aspects in the "in ending" section
+            if (inEnding)
+            {
+                if (spawner != null)
+                {
+                    if (Keyboard.current[Key.Numpad1].wasReleasedThisFrame)
+                    {
+                        WarpToPlanet(AstroObjectLocator.GetAstroObject("QM Eye Aspect"));
+                    }
+                    if (Keyboard.current[Key.Numpad2].wasReleasedThisFrame)
+                    {
+                        spawner.DebugWarp(InEndingPropsController.stationSpawnPoint);
+                    }
+                    if (Keyboard.current[Key.Numpad3].wasReleasedThisFrame)
+                    {
+                        WarpToPlanet(AstroObjectLocator.GetAstroObject("QM Brittle Hollow Aspect"));
+                    }
+                    if (Keyboard.current[Key.Numpad4].wasReleasedThisFrame)
+                    {
+                        WarpToPlanet(AstroObjectLocator.GetAstroObject("QM Dark Bramble Aspect"));
+                    }
+                    if (Keyboard.current[Key.Numpad5].wasReleasedThisFrame)
+                    {
+                        WarpToPlanet(AstroObjectLocator.GetAstroObject("QM Giant's Deep Aspect"));
+                    }
+                    if (Keyboard.current[Key.Numpad6].wasReleasedThisFrame)
+                    {
+                        WarpToPlanet(AstroObjectLocator.GetAstroObject("QM Hourglass Twins Aspect"));
+                    }
+                }
+            }
+            else
+            {
+                if (Keyboard.current[Key.Numpad1].wasReleasedThisFrame)
+                {
+                    if (!inEnding)
+                    {
+                        AltEnding.newHorizonsAPI.ChangeCurrentStarSystem("clay.AltEnding");
+                    }
+                }
+            }
+        }
+
+        private static void WarpToPlanet(AstroObject planet, float offset = 200f)
+        {
+            var player = Locator.GetPlayerBody();
+            var newWorldPos = planet.transform.position + Vector3.up * offset;
+
+            player.WarpToPositionRotation(newWorldPos, Quaternion.identity);
+            player.SetVelocity(planet.GetAttachedOWRigidbody().GetPointVelocity(newWorldPos));
+        }
+    }
+}
